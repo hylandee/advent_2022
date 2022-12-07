96ebe9d07472757bdb08fc5ece97ab70ccb174c3
Dylan Howell <dylanleehowell@gmail.com>
2022-12-06 21:43:26 -0600

    Day 4 part 2 imeanit

--- src/days/day4/part1.rs
+++ src/days/day4/part1.rs
@@ -2,7 +2,7 @@ pub fn solve(input: String) {
     let mut sum = 0;
     for line in input.lines() {
         let mut split = line.split(',');
-        let(left, right) = (split.next().unwrap(), split.next().unwrap());
+        let (left, right) = (split.next().unwrap(), split.next().unwrap());
         if are_either_contained(left, right) {
             sum += 1;
         }
@@ -17,10 +17,12 @@ fn are_either_contained(left: &str, right: &str) -> bool {
 fn left_in_right(left: &str, right: &str) -> bool {
     let (left, right) = (get_min_max(left), get_min_max(right));
     left.0 >= right.0 && left.1 <= right.1
-    
 }
 
 fn get_min_max(input: &str) -> (i32, i32) {
     let mut split = input.split('-');
-    (split.next().unwrap().parse::<i32>().unwrap(), split.next().unwrap().parse::<i32>().unwrap())
+    (
+        split.next().unwrap().parse::<i32>().unwrap(),
+        split.next().unwrap().parse::<i32>().unwrap(),
+    )
 }
--- src/days/day4/part2.rs
+++ src/days/day4/part2.rs
@@ -1,3 +1,24 @@
 pub fn solve(input: String) {
-    
+    let mut sum = 0;
+    for line in input.lines() {
+        let mut split = line.split(',');
+        let (left, right) = (split.next().unwrap(), split.next().unwrap());
+        if any_overlap(left, right) {
+            sum += 1;
+        }
+    }
+    println!("sum of complete overlappers: {sum}");
+}
+
+fn any_overlap(left: &str, right: &str) -> bool {
+    let (left, right) = (get_min_max(left), get_min_max(right));
+    (left.1 >= right.0 && left.0 <= right.1) || (right.1 >= left.0 && right.0 <= left.1)
+}
+
+fn get_min_max(input: &str) -> (i32, i32) {
+    let mut split = input.split('-');
+    (
+        split.next().unwrap().parse::<i32>().unwrap(),
+        split.next().unwrap().parse::<i32>().unwrap(),
+    )
 }
